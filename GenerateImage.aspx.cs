0b371c0147f34a30ff3bbb761086b015b0d0f71b
Shahzad Latif <shahzad.latif@aspose.com>
2019-10-23 17:39:47 +0500

    Update GenerateImage.aspx.cs

--- GenerateImage.aspx.cs
+++ GenerateImage.aspx.cs
@@ -26,16 +26,22 @@ namespace SlideImageGenerator
                 Presentation pres = new Presentation();
                 ISlide slide = pres.Slides.AddEmptySlide(pres.LayoutSlides.GetByType(SlideLayoutType.Title));
 
+                //next, we take some text as input. You can use as much text as appropriate in your case. 
                 //Setting title as author
                 ((IAutoShape)slide.Shapes[0]).TextFrame.Text = TextBoxAuthor.Text;
 
                 //Setting session as sub-tilte
                 ((IAutoShape)slide.Shapes[1]).TextFrame.Text = TextBoxSession.Text;
 
+                //You can also set scaling factor by your choice with Aspose.Slides API
                 float ScaleFactor = 1;
                 var image =slide.GetThumbnail(ScaleFactor, ScaleFactor);
+                
+                //the resultant image created from the text is saved to memory stream, but you can save it to disk or storage as well
                 MemoryStream mem=SaveAsImage(image);
 
+        
+                //the image memory stream is rendered to the user and the user can see the output image 
                 Response.Clear();
                 Response.ContentType = "Application/png";
                 Response.AppendHeader("Content-Disposition", "attachment; filename="+TextBoxAuthor.Text+".png");
